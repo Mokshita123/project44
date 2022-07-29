6cab2788eb4d203b82047ae96a6ed63b2ba23877
Mokshita123 <81616066+Mokshita123@users.noreply.github.com>
2022-07-29 13:52:11 +0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var w1,w2,w3,w4,w5,w6,w7,w8,w9,w10
+var w11,w12,w13,w14,w15,w16,w17,w18,w19,w20
+var w31,w32,w33,w34,w35,w36,w37,w38,w39,w40
+
+function setup() {
+  createCanvas(700,600);
+  w1 =createSprite(330, 50, 600, 5);
+  w2 =createSprite(32,300, 5, 502);
+  w3 =createSprite(330, 550, 600, 5);
+  w4 =createSprite(630,260, 5, 425);
+  w5 =createSprite(320, 120, 444, 5);
+  w6 =createSprite(100,163,5, 90);
+  w7 =createSprite(100,315,5, 60);
+  w8 =createSprite(100,450,5, 60);
+  w9 =createSprite(68,283, 69, 5);
+  w10 =createSprite(159,478, 120, 5);
+  w11 =createSprite(360,478, 120, 5);
+  w12 =createSprite(530,478,50, 5);
+  w13 =createSprite(555,380, 5, 200);
+  w14 =createSprite(590,282, 74, 5);
+  w15 =createSprite(555,200, 5, 30);
+  w16 =createSprite(220, 200, 80, 5);
+  w17 =createSprite(410,200, 120, 5);
+  w18 =createSprite(470,298, 5, 200);
+  w19 =createSprite(180,298, 5, 200);
+  w20 =createSprite(325,400, 295, 5);
+  w21 =createSprite(325, 260, 170, 5);
+  w22 =createSprite(407,305, 5, 90);
+  w23 =createSprite(240,303, 5, 90);
+  w24 =createSprite(288, 350, 100, 5);
+  w25 =createSprite(330,440, 5, 74);
+  w26 =createSprite(260,160, 5, 78);
+  w27 =createSprite(515,350, 82, 5);
+
+}
+
+function draw() {
+  background("black");  
+  drawSprites();
+  
+}
